057470548da3989aa16f8fd34b2d1743cd9170c4
cyanide17 <cyanide17@snu.ac.kr>
2021-01-26 14:56:00 +0900

    parse function detach

--- src/parser.c
+++ src/parser.c
@@ -26,10 +26,10 @@ struct op_log {
 };
 
 typedef struct _DMA_block{
-	uint32_t base;
-    uint32_t vendor; // vendor id for device identification
-    uint32_t device; // device id for device identification
-	LIST_ENTRY(_DMA_block) ptr;
+    uint32_t base;                   /* base address of allocated page */
+    uint32_t vendor;                 /* vendor id for device identification */
+    uint32_t device;                 /* device id for device identification */
+	LIST_ENTRY(_DMA_block) ptr;      /* list node */
 }DMA_blk;
 
 DMA_blk *create_DMA_blk(uint32_t base, uint32_t vendor, uint32_t device);
@@ -37,6 +37,9 @@ LIST_HEAD(DMA_list,_DMA_block) DMA_blks;
 
 #define PAGE_SIZE 1UL << 12
 #define DATA_SIZE PAGE_SIZE<<8
+
+#define DMASTART 0x10000000
+#define DMASTOP  0x1FF00000
 
 #define OPTYPE_NONE 0x0
 #define OPTYPE_PIO 0x1
@@ -51,20 +54,19 @@ void printf_width(int op_size);
 void printf_readin(uint16_t op_type, uint16_t is_pci,uint16_t bar_idx,uint32_t addr);
 void printf_writeout(uint16_t op_type, uint16_t is_pci,uint32_t val,uint16_t bar_idx,uint32_t addr);
 void printf_dyanamic_writeout(uint16_t op_type, int op_size, uint16_t is_pci, uint32_t val, uint16_t bar_idx, uint32_t addr);
+void parse(struct op_log *log);
 
 int main(int argc, char** argv)
 {
     int size = sizeof(struct op_log);
     struct op_log *log;
     unsigned int offset = 0;
-    int op_size;
     long lSize;
     char *buffer;
     size_t remain;
     unsigned int vid = -1;
     unsigned int did = -1;
     int dma_exists = 0;
-    uint32_t last_log_addr = -1;
 
     FILE *fp = fopen(argv[1], "r");
     if(fp == NULL) {
@@ -95,38 +97,38 @@ int main(int argc, char** argv)
 	LIST_INIT(&DMA_blks);
     log = (void*)(buffer + offset);
     for(; log->op_type != OPTYPE_NONE; offset+=size){
+
+        /* if operation is 'DMA_read' or 'DMA_write' */
         if((log->op_type == OPTYPE_DMA)&&(log->is_alloc == 0)&&(log->is_free == 0)){
-            // if operation is 'DMA_read' or 'DMA_write'
             dma_exists = 1;
             int blk_exists = 0;
             int base = (log->addr)&(~0xfff);
 
-            // check if target address of the operation is already allocated
+            /* check if target address of the operation is already allocated */
             DMA_blk *blk;
             LIST_FOREACH(blk, &DMA_blks, ptr){
-                if(blk->base == base)
-                {
+                if(blk->base == base){
                     blk_exists = 1;
                     break;
                 }
             }
 
-            // if target address of the operation is not allocated
+            /* if target address of the operation is not allocated */
             if(!blk_exists){
                 DMA_blk *new_blk = create_DMA_blk(base,log->vendor,log->device);
                 LIST_INSERT_HEAD(&DMA_blks,new_blk,ptr);
 
-                // get `struct pci_dev` using vendor & device id
+                /* get `struct pci_dev` using vendor & device id */
                 printf("pdev = pci_get_device(0x%x,0x%x,NULL);\n",log->vendor,log->device);
 
-                // allocate one page(4KB) for that device
-                // &
-                // get virtual address(cpu_addr) from return value
-                // &
-                // get bus address(dma_handle) by pointer argument
+                /*
+                 * - allocate one page(4KB) for that device
+                 * - get virtual address(cpu_addr) from return value
+                 * - get bus address(dma_handle) by pointer argument
+                 */
                 printf("cpu_addr = dma_alloc_coherent(&(pdev->dev),0x1000, &dma_handle, GFP_KERNEL);\n");
 
-                // record info. at dynamic kernel list
+                /* record info. at dynamic kernel list */
                 printf("add_DMA_blk(cpu_addr,0x%x,dma_handle);\n",base);
                 printf("\n");
             }
@@ -143,12 +145,14 @@ int main(int argc, char** argv)
     log = (void*)(buffer + offset);
     offset += size;
     for(; log->op_type != OPTYPE_NONE; offset+=size) {
+
+        /*
+         * load BAR addresses of pci device
+         *    if target device is not set (vid == -1)
+         *    or if target device has changed (vid != log->vendor)
+         */
         if((log->is_pci > 0)&&(log->op_type != OPTYPE_NEXT)){
             if((vid == -1) || (vid != log->vendor)){
-                // load BAR addresses of device
-                // if target device is not set (vid == -1)
-                // or
-                // if target device has changed (vid != log->vendor),
                 vid = log->vendor;
                 did = log->device;
                 printf("dev = load_bar(0x%x,0x%x);\n",vid,did);
@@ -156,78 +160,13 @@ int main(int argc, char** argv)
             }
         }
 
-        op_size = (int)pow(2,log->op_size);
-        switch(log->op_type) {
-            case OPTYPE_PIO:
-                if(log->op_rw == OPRW_READ) {
-                    printf("in");
-                    printf_width(op_size);
-                    printf_readin(OPTYPE_PIO, log->is_pci, log->bar_idx, log->addr);
+        /*
+         * parse operation
+         */
+        parse(log);
 
-                } else {
-                    if((log->val>=0x10000000)&&(log->val<=0x1FF00000)&&(((log->val)&0xFFF)==0x000))
-                    {
-                        printf_dyanamic_writeout(OPTYPE_PIO, op_size, log->is_pci, log->val, log->bar_idx, log->addr);
-                    }else
-                    {
-                        printf("out");
-                        printf_width(op_size);
-                        printf_writeout(OPTYPE_PIO, log->is_pci, log->val, log->bar_idx, log->addr);
-                    }
-                }
-                break;
-            case OPTYPE_MMIO:
-                if(log->is_pci>0){
-                    if(log->op_rw == OPRW_READ) {
-                        printf("read");
-                        printf_width(op_size);
-                        printf_readin(OPTYPE_MMIO, log->is_pci, log->bar_idx, log->addr);
-                    } else {
-                        if((log->val>=0x10000000)&&(log->val<=0x1FF00000)&&(((log->val)&0xFFF)==0x000))
-                        {
-                            printf_dyanamic_writeout(OPTYPE_MMIO, op_size, log->is_pci, log->val, log->bar_idx, log->addr);
-                        }else
-                        {
-                            printf("write");
-                            printf_width(op_size);
-                            printf_writeout(OPTYPE_MMIO, log->is_pci, log->val, log->bar_idx, log->addr);
-                        }
-                    }
-                }
-                break;
-            case OPTYPE_DMA:
-                dma_exists = 1;
-                if((log->is_alloc == 0) && (log->is_free == 0))
-                {
-                    if((last_log_addr == -1) || (last_log_addr != ((log->addr)&(~0xFFF)))){
-                        printf("blk = get_DMA_blk(0x%x);\n",(log->addr)&(~0xFFF));
-                        last_log_addr = (log->addr)&(~0xFFF);
-                    }
-                    if(log->op_rw == OPRW_READ)
-                    {
-                        printf("read");
-                        printf_width(op_size);
-                        printf("(blk->cpu_addr);\n");
-                    }
-                    else
-                    {
-                        printf("write");
-                        printf_width(op_size);
-                        printf("(0x%x, blk->cpu_addr);\n", log->val);
-                    }
-                }
-
-                break;
-            case OPTYPE_NEXT:
-                printf("\n");
-                break;
-            default:
-                break;
-
-        }
         log = (void*)(buffer + offset);
     }
-
     fclose(fp);
 
     if(dma_exists)
@@ -238,9 +177,7 @@ int main(int argc, char** argv)
             printf("blk = get_DMA_blk(0x%x);\n", blk->base);
             printf("dma_free_coherent(&(pdev->dev), 0x1000, blk->cpu_addr, blk->dma_handle);\n");
         }
-
     }
-
     return 0;
 }
 
@@ -248,13 +185,82 @@ int main(int argc, char** argv)
 
 
 
+/*
+ * functions
+ */
+
+
+void parse(struct op_log *log)
+{
+    int op_size = (int)pow(2,log->op_size);
+    static uint32_t last_log_addr = -1;
+
+    switch(log->op_type){
+        case OPTYPE_PIO:
+            if(log->op_rw == OPRW_READ){
+                printf("in");
+                printf_width(op_size);
+                printf_readin(OPTYPE_PIO, log->is_pci, log->bar_idx, log->addr);
+            } else{
+                if((log->val>=DMASTART)&&(log->val<=DMASTOP)&&(((log->val)&0xFFF)==0x000))
+                    printf_dyanamic_writeout(OPTYPE_PIO, op_size, log->is_pci, log->val, log->bar_idx, log->addr);
+                else{
+                    printf("out");
+                    printf_width(op_size);
+                    printf_writeout(OPTYPE_PIO, log->is_pci, log->val, log->bar_idx, log->addr);
+                }
+            }
+            break;
+        case OPTYPE_MMIO:
+            if(log->is_pci>0){
+                if(log->op_rw == OPRW_READ){
+                    printf("read");
+                    printf_width(op_size);
+                    printf_readin(OPTYPE_MMIO, log->is_pci, log->bar_idx, log->addr);
+                } else{
+                    if((log->val>=DMASTART)&&(log->val<=DMASTOP)&&(((log->val)&0xFFF)==0x000))
+                        printf_dyanamic_writeout(OPTYPE_MMIO, op_size, log->is_pci, log->val, log->bar_idx, log->addr);
+                    else{
+                        printf("write");
+                        printf_width(op_size);
+                        printf_writeout(OPTYPE_MMIO, log->is_pci, log->val, log->bar_idx, log->addr);
+                    }
+                }
+            }
+            break;
+        case OPTYPE_DMA:
+            if((log->is_alloc == 0) && (log->is_free == 0)){
+                if((last_log_addr == -1) || (last_log_addr != ((log->addr)&(~0xFFF)))){
+                    printf("blk = get_DMA_blk(0x%x);\n",(log->addr)&(~0xFFF));
+                    last_log_addr = (log->addr)&(~0xFFF);
+                }
+                if(log->op_rw == OPRW_READ){
+                    printf("read");
+                    printf_width(op_size);
+                    printf("(blk->cpu_addr);\n");
+                } else{
+                    printf("write");
+                    printf_width(op_size);
+                    printf("(0x%x, blk->cpu_addr);\n", log->val);
+                }
+            }
+            break;
+        case OPTYPE_NEXT:
+            printf("\n");
+            break;
+        default:
+            break;
+    }
+
+}
+
 
 DMA_blk *create_DMA_blk(uint32_t base, uint32_t vendor, uint32_t device)
 {
-	DMA_blk *blk;
-	if((blk = (DMA_blk *)malloc(sizeof(DMA_blk))) != NULL)
-	{
-		blk->base = base;
+    DMA_blk *blk;
+    if((blk = (DMA_blk *)malloc(sizeof(DMA_blk))) != NULL)
+    {
+        blk->base = base;
         blk->vendor = vendor;
         blk->device = device;
 	}
